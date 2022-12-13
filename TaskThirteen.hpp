690e88fac0637f77a29fa9622c91995f30615718
Paweł Więcław <pawciow123@gmail.com>
2022-12-13 13:39:22 +0100

    First star done

--- TaskThirteen.hpp
+++ TaskThirteen.hpp
@@ -11,14 +11,13 @@
 #include <optional>
 #include <variant>
 
+// Unfortunately the parsing was a nightmare, so I looked up to solution https://gist.github.com/ParadoxsHorizon/a5a783a3d867cc9151f0bc70a4bb4559#file-day13-cpp
 class TaskThirteen : public TaskBase {
 
     struct Packets : std::variant<unsigned int, std::vector<Packets>>{
 
         Packets* parent;
-
         using std::variant<unsigned int, std::vector<Packets>>::variant;
-
         std::vector<Packets>& getList()
         {
             return std::get<std::vector<Packets>>(*this);
@@ -43,11 +42,49 @@ class TaskThirteen : public TaskBase {
             s << "]";
             return s;
         }
+
+        std::weak_ordering compare(Packets& other) {
+            if(index()==0 and other.index()==0)
+                return getInt() <=> other.getInt();
+
+            std::vector<Packets> left;
+            if(index() == 0)
+            {
+                left.push_back(getInt());
+            } else{
+                left = this->getList();
+            }
+
+            std::vector<Packets> right;
+            if(other.index() == 0)
+            {
+                right.push_back(other.getInt());
+            } else{
+                right = other.getList();
+            }
+
+            auto size = std::min(right.size(), left.size());
+            for(auto i = 0; i < size; i++)
+            {
+                auto ordering = left[i].compare(right[i]);
+                if(ordering != std::weak_ordering::equivalent)
+                {
+                    return ordering;   // It will return if items are not equal
+                }
+            }
+
+            if(left.size() < right.size())
+                return std::weak_ordering::less;
+            if(left.size() > right.size())
+                return std::weak_ordering::greater;
+
+            return std::weak_ordering::equivalent;
+        }
     };
 
     void assertResults() override {
         auto firstResult = solveFirstTask();
-        assert(firstResult == 13); //
+        assert(firstResult == 6415); //
 
 //        auto secondResult = solveSecondTask();
 //        assert(secondResult == 39109444654);
@@ -66,11 +103,22 @@ class TaskThirteen : public TaskBase {
     }
 
     virtual unsigned int solveFirstTask() override {
-        unsigned int result = 1;
+        unsigned int result = 0;
+        m_data = {};
         readData();
         parseData();
-        for(auto el: m_data)
-            std::cout << el;
+
+        unsigned int pairNumber=0;
+        for(unsigned int i = 0; i < m_data.size(); i+=2)
+        {
+            pairNumber++;
+            auto ordering = m_data[i].compare(m_data[i+1]);
+            if(ordering == std::weak_ordering::less)
+            {
+                result += pairNumber;
+                std::cout << "Pair number " << pairNumber << " is correct" << std::endl;
+            }
+        }
         std::cout << "For my first star result is: " << result << std::endl;
         return result;
     }
@@ -120,8 +168,6 @@ private:
             if(line[it] == ',' or line[it] == ']')
             {
                 std::string num(line.begin()+j, line.begin()+it);
-//                std::cout << "Before: " << num << std::endl;
-//
                 while (num.starts_with(',') or num.starts_with('['))
                     num.erase(0,1);
                 std::cout << num << std::endl;
@@ -140,6 +186,8 @@ private:
         std::stringstream ss;
         for(auto& line : m_input)
         {
+            if(line == "\r")
+                continue;
             line.pop_back(); // to remove newline
             Packets packets = std::vector<Packets>{};
 
@@ -152,25 +200,6 @@ private:
             }
             m_data.push_back(packets);
         }
-    }
-//    unsigned int findBeginofNextList(std::string& line)
-//    {
-//        for(int i = 0; i < line.size(); i++)
-//        {
-//            if(line[i] == '[')
-//                return i;
-//            throw std::runtime_error("Messed up finding begin of this list!");
-//        }
-//    }
-    unsigned int findEndOfNextList(std::string& line)
-    {
-        for(int i = 0; i <line.size(); i++)
-        {
-            auto character = line[i];
-            if(character == ']')
-                return i;
-        }
-        throw std::runtime_error("Messed up finding end of this list!");
     }
 
     std::vector<std::string> split (const std::string &s, char delim) {
